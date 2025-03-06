37874912a7d418860ef927ddad9afad93cfa9345
horea <horea@rospace.com>
2025-03-06 17:04:02 +0200

    Issue #30: Replace Psalm with PHPStan
    Signed-off-by: horea <horea@rospace.com>

--- src/Assertion/AssertionPluginManager.php
+++ src/Assertion/AssertionPluginManager.php
@@ -11,6 +11,6 @@ use Laminas\ServiceManager\AbstractPluginManager;
  */
 class AssertionPluginManager extends AbstractPluginManager
 {
-    /** @var string */
+    /** @var string  */
     protected $instanceOf = AssertionInterface::class;
 }
